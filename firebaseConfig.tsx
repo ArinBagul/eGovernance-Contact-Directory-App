246b25a18deab810f01ab2e83f33b4d1646f103b
Arin Bagul <prototypearin@gmail.com>
2024-05-03 22:18:00 +0530

    adding config

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCSCJZYUQvQZ7_ZP_nSUqbqGtFVz6WF-Ho",
+  authDomain: "egovernance-cf66d.firebaseapp.com",
+  databaseURL: "https://egovernance-cf66d-default-rtdb.firebaseio.com",
+  projectId: "egovernance-cf66d",
+  storageBucket: "egovernance-cf66d.appspot.com",
+  messagingSenderId: "22310314453",
+  appId: "1:22310314453:web:1cb78be2d297389d0f9d5c",
+  measurementId: "G-D7TZ864CW3"
 };
 
 // Initialize Firebase
